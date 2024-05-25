9b6f23851ddda67abd866d13680a28613e44d5a7
Yarolay22 <142624250+Yarolay22@users.noreply.github.com>
2024-05-25 10:08:55 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'inventario_app-moviles',
+  webDir: 'dist'
+};
+
+export default config;
